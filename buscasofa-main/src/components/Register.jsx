1c70359b6d0f1a15a160648cb0ec22e7069e02ab
Francisco Javier Gumiel espinosa <fcojaviergumiel@gmail.com>
2026-06-06 15:40:20 +0200

    Adding docker compose

--- buscasofa-main/src/components/Register.jsx
+++ buscasofa-main/src/components/Register.jsx
@@ -10,7 +10,7 @@ function Register() {
   const handleSubmit = async e => {
     e.preventDefault();
     setMsg('');
-    const res = await fetch('http://localhost:4000/api/register', {
+    const res = await fetch('/api/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(form)
